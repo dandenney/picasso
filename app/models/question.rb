b3d770c5b07a4d66ff2afac8d4c41619c4347c27
Dan Denney <dd.subscriptions@gmail.com>
2015-05-09 15:54:36 -0400

    Add association between question and user

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ActiveRecord::Base
+
+  belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509192209) do
+ActiveRecord::Schema.define(version: 20150509194801) do
 
   create_table "installs", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(version: 20150509192209) do
     t.string   "category"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

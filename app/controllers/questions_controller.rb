502adeb880504d96930e48ae0eabcac2c77206d0
Dan Denney <dd.subscriptions@gmail.com>
2015-05-09 16:13:10 -0400

    Add authorization to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index, :show]
 
   # GET /questions
   # GET /questions.json
@@ -14,7 +15,7 @@ class QuestionsController < ApplicationController
 
   # GET /questions/new
   def new
-    @question = Question.new
+    @question = current_user.questions.build
   end
 
   # GET /questions/1/edit
@@ -24,7 +25,7 @@ class QuestionsController < ApplicationController
   # POST /questions
   # POST /questions.json
   def create
-    @question = Question.new(question_params)
+    @question = current_user.questions.build(question_params)
 
     respond_to do |format|
       if @question.save
